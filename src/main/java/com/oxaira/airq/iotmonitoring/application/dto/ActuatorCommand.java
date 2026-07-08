b0499593ae4196b40154e7f4088df5f37136580e
Andres <83299190+Andres-0304@users.noreply.github.com>
2026-07-07 20:15:58 -0500

    feat: add ActuatorCommand DTO for IoT device control signals

--- src/main/java/com/oxaira/airq/iotmonitoring/application/dto/ActuatorCommand.java
+++ src/main/java/com/oxaira/airq/iotmonitoring/application/dto/ActuatorCommand.java
@@ -1,5 +1,6 @@
 package com.oxaira.airq.iotmonitoring.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,13 @@ import lombok.NoArgsConstructor;
 public class ActuatorCommand {
     private boolean extractor;
     private boolean hepa;
+    
+    @JsonProperty("ac_cool")
     private boolean ac_cool;
+    
+    @JsonProperty("ac_dry")
     private boolean ac_dry;
+    
+    @JsonProperty("dampers_open")
     private boolean dampers_open;
 }
